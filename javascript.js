9aead4590ac316adad2f153b588a3d7bc25c3d6a
Sheronne <sheronne@hotmail.fr>
2018-03-05 12:33:34 +0100

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -2,10 +2,13 @@ var date = new Date();
 var tableau = new Vue({
     el: '#main',
     data: {
+        elementsPerPage: 10,
+        elementsPerPageOptions: [10, 20, 30],
         currentPage: 1,
-        elementsPerPage: 20,
         ascending: false,
         sortColumn: '',
+        showModal: false,
+        // filOptions:['ID','Titre','Resume','Affectation','Client','Etat'],
         col: {
             ID: '',
             Titre: '',
@@ -13,7 +16,7 @@ var tableau = new Vue({
             Affectation: '',
             Client: '',
             Etat: '',
-            Supprimer: ''
+            Date: '',
         },
         row: {
             id: '',
@@ -22,9 +25,10 @@ var tableau = new Vue({
             affectation: '',
             client: '',
             etat: '',
-            date: date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes()
+            date: date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes(),
         },
         rows: []
+
     },
     mounted: function () {
         this.on_load()
@@ -36,13 +40,48 @@ var tableau = new Vue({
             localStorage.setItem('allrows', JSON.stringify(this.rows));
         },
         "on_load": function on_load() {
-            if (JSON.parse(localStorage.getItem('allrows')) != null)
+            if (JSON.parse(localStorage.getItem('allrows')) != null) {
                 this.rows = JSON.parse(localStorage.getItem('allrows'));
+            }
+            // for (var i = 0; i <= this.rows.length; i++) {
+            //     this.row.supprimer.innerHTML = '<input type="checkbox" name="del"/>';
+            // }
         },
         "onSubmit": function onSubmit() {
         },
         "removeRow": function removeRow(index) {
-            this.itemList.splice(index, 1)
+            ///var result = window.confirm("You are about to delete select rows. Are you sure?");
+            var result = true;
+            if (result) {
+                var index = this.rows.indexOf(index);
+                if (index === -1) {
+                    return;
+                }
+                this.rows.splice(index, 1);
+            }
+            localStorage.setItem('allrows', JSON.stringify(this.rows));
+        },
+        // "removeRow": function removeRow(checkbox) {
+        //     for (var i = 0; i <= this.rows.length; i++) {
+        //         let checkbox = document.querySelector('#delbox');
+        //         console.log(checkbox);
+        //         if (checkbox) {
+        //             this.$delete(this.rows);
+        //         }
+        //     }
+        //     localStorage.setItem('allrows', JSON.stringify(this.rows));
+        // },
+        // "removeRow": function removeRow(index) {
+        //     for (let i = this.rows.length - 1; i = 0; i--) {
+        //         if (this.rows[i].id === id) {
+        //             this.$delete(this.rows, index);
+        //         }
+        //     }
+        //     localStorage.setItem('allrows', JSON.stringify(this.rows));
+        // },
+        "removeThisRow": function removeThisRow(index) {
+            this.$delete(this.rows, index);
+            localStorage.setItem('allrows', JSON.stringify(this.rows));
         },
         "sortTable": function sortTable(col) {
             if (this.sortColumn === col) {
@@ -61,24 +100,59 @@ var tableau = new Vue({
                 return 0;
             })
         },
+        // "searchArray": function searchArray() {
+        //     // Declare variables
+        //     var input, filter, table, tr, td, i;
+        //     input = document.getElementById("myInput");
+        //     filter = input.value.toUpperCase();
+        //     table = document.getElementById("tab");
+        //     tr = table.getElementsByTagName("tr");
+        //     // Loop through all table rows, and hide those who don't match the search query
+        //     for (i = 0; i < this.rows.length; i++) {
+        //         td = this.rows.length[0];
+        //         if (td) {
+        //             if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        //                 tr[i].style.display = "";
+        //             } else {
+        //                 tr[i].style.display = "none";
+        //             }
+        //         }
+        //     }
+        // },
+        "resPage": function resPage() {
+            this.currentPage = 1;
+        },
         "num_pages": function num_pages() {
             return Math.ceil(this.rows.length / this.elementsPerPage);
         },
         "get_rows": function get_rows() {
-            var start = (this.currentPage - 1) * this.elementsPerPage;
+            const start = (this.currentPage - 1) * this.elementsPerPage;
             var end = start + this.elementsPerPage;
             return this.rows.slice(start, end);
         },
         "change_page": function change_page(page) {
             this.currentPage = page;
-        },
+        }
     },
     computed: {
         "columns": function columns() {
             if (this.rows.length == 0) {
-                return [];
+                return ['ID', 'Titre', 'Resume', 'Affectation', 'Client', 'Etat', 'Date'];
             }
             return Object.keys(this.rows[0])
-        }
+        },
     }
 });
+
+// register modal component
+Vue.component('modal', {
+    template: '#modal-template'
+  })
+  
+  // start app
+  new Vue({
+    el: '#app',
+    data: {
+      showModal: false
+    }
+  })
